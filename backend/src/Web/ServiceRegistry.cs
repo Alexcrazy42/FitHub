b722a65bb77f82e09ae73f70bf01faf91ebe726c
Alex42 <alexcrazy42@mail.ru>
2025-08-09 19:19:11 +0300

    +

--- backend/src/Web/ServiceRegistry.cs
+++ backend/src/Web/ServiceRegistry.cs
@@ -6,6 +6,7 @@ public static class ServiceRegistry
 {
     public static void AddWeb(this IServiceCollection services)
     {
+
         services
             .AddMvcCore()
             .AddApiExplorer()
