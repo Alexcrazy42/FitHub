d5337ed41e843c2a303dbc8c209a7c110a7424b0
Alex42 <alexcrazy42@mail.ru>
2025-09-01 00:11:10 +0300

    +

---
+++ sandbox/Hangfire/HangfireJobs/MyJobs.cs
@@ -0,0 +1,11 @@
+﻿namespace Hangfire;
+
+public static class MyJobs
+{
+    public static void DoWork()
+    {
+        var nodeId = Environment.GetEnvironmentVariable("NODE_ID") ?? "unknown";
+        Console.WriteLine($"[Job] Выполняется на ноде {nodeId} в {DateTime.Now}");
+    }
+}
+
