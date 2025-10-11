3ed2d48b8408392344d053f89a96976443023390
Alex42 <alexcrazy42@mail.ru>
2025-10-11 23:04:09 +0300

    +

---
+++ frontend/src/types/files.ts
@@ -0,0 +1,15 @@
+﻿export interface IPresignedUrlResponse {
+    fileId: string;
+    url: string;
+    objectKey: string;
+}
+
+export enum EntityType {
+    Gym = "Gym"
+}
+
+export interface IMakeFilesActiveRequest {
+    fileIds: string[],
+    entityId: string;
+    entityType: EntityType
+}
