30f909a944c3cfe34160f5f8629f2e7b4b7b5968
Alex42 <alexcrazy42@mail.ru>
2026-03-02 22:41:59 +0300

    init

---
+++ backend/src/Domain/Messaging/Todo.cs
@@ -0,0 +1,14 @@
+﻿namespace FitHub.Domain.Messaging;
+
+// стикер, гифка (возможно имеет смысл объединить в одну модельку (подумать)
+// 1. админка для CMS Admin где он прям будет управлять стикерами и гифками
+// 2. для пользователя: как в вк - раздел где можно добавить к себе стикеры и гифки
+
+// модельки:
+// sticker_group (id, name, cost?), sticker (id, name, group_id, s3path)
+// gif: id, name, s3path
+
+public class Todo
+{
+
+}
