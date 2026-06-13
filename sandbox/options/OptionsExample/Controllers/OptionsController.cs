d8b213d6413f8d6c40df797eeeb304cef4da4508
Alex42 <alexcrazy42@mail.ru>
2026-06-14 01:46:07 +0300

    +

---
+++ sandbox/options/OptionsExample/Controllers/OptionsController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using OptionsExample.Options;
+
+namespace OptionsExample.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class OptionsController : ControllerBase
+{
+    private readonly SomeOptions someOptions;
+    private readonly SomeOptions someOptionsFromSnap;
+    private readonly SomeOptions someOptionsFromMonitor;
+
+    public OptionsController(IOptions<SomeOptions> someOptions,
+        IOptionsSnapshot<SomeOptions> optionsSnapshot,
+        IOptionsMonitor<SomeOptions> optionsMonitor)
+    {
+        this.someOptions = someOptions.Value;
+        this.someOptionsFromSnap = optionsSnapshot.Value;
+        this.someOptionsFromMonitor = optionsMonitor.CurrentValue;
+    }
+
+    [HttpGet("options")]
+    public SomeOptions GetOptions()
+    {
+        return someOptions;
+    }
+
+    [HttpGet("snap")]
+    public SomeOptions GetOptionsFromSnap()
+    {
+        return someOptionsFromSnap;
+    }
+
+    [HttpGet("monitor")]
+    public SomeOptions GetOptionsFromMonitor()
+    {
+        return someOptionsFromMonitor;
+    }
+}
---
+++ sandbox/options/OptionsExample/Options/SomeOptions.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OptionsExample.Options;
+
+//        "SomeOptions__SomeIntDictionary__one": "123"
+
+public class SomeOptions
+{
+    public const string Key = "SomeOptions";
+
+    [Required]
+    public string SomeString { get; set; } = null!;
+    
+    [Required]
+    public int SomeInt { get; set; }
+    
+    [Required]
+    public bool SomeBool { get; set; }
+
+    [Required]
+    public NestedOptions Nested { get; set; } = null!;
+
+    [MinLength(2)]
+    public List<int> SomeIntList { get; set; } = [];
+    
+    [Required]
+    public Dictionary<string, int> SomeIntDictionary { get; set; } = null!;
+}
+
+public class NestedOptions
+{
+    [Required]
+    public int NestedInt { get; set; }
+}
