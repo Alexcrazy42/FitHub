ff8f24415cd3258364d8ac76f98245ce856721b9
Alex42 <alexcrazy42@mail.ru>
2026-06-21 19:55:03 +0300

    add docker compose

---
+++ frontend/docker-entrypoint.sh
@@ -0,0 +1,8 @@
+﻿#!/bin/sh
+# docker-entrypoint.sh
+
+# Заменяем плейсхолдеры в собранных JS файлах
+find /usr/share/nginx/html -type f -name "*.js" -exec sed -i \
+  "s|VITE_API_URL|${VITE_API_URL:-http://platform-api}|g" {} \;
+
+exec nginx -g "daemon off;"
--- frontend/src/api/useApiService.tsx
+++ frontend/src/api/useApiService.tsx
@@ -2,13 +2,25 @@ import { useMemo } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { ApiService } from './ApiService';
 
-export const API_URL = import.meta.env?.VITE_API_URL
-  ? `${import.meta.env?.VITE_API_URL}/api`
-  : (() => { throw new Error("VITE_API_URL не задан"); })();
+// export const API_URL = import.meta.env?.VITE_API_URL
+//   ? `${import.meta.env?.VITE_API_URL}/api`
+//   : (() => { throw new Error("VITE_API_URL не задан"); })();
 
-export const API_URL_CLEAN = import.meta.env?.VITE_API_URL
-  ? `${import.meta.env?.VITE_API_URL}`
-  : (() => { throw new Error("VITE_API_URL не задан"); })();
+export const API_URL = window._env_?.VITE_API_URL
+  ? `${window._env_.VITE_API_URL}/api`
+  : import.meta.env?.VITE_API_URL
+    ? `${import.meta.env.VITE_API_URL}/api`
+    : (() => { throw new Error("VITE_API_URL не задан"); })();
+
+export const API_URL_CLEAN = window._env_?.VITE_API_URL
+  ? `${window._env_.VITE_API_URL}`
+  : import.meta.env?.VITE_API_URL
+    ? `${import.meta.env.VITE_API_URL}`
+    : (() => { throw new Error("VITE_API_URL не задан"); })();
+
+// export const API_URL_CLEAN = import.meta.env?.VITE_API_URL
+//   ? `${import.meta.env?.VITE_API_URL}`
+//   : (() => { throw new Error("VITE_API_URL не задан"); })();
 
 export const useApiService = () => {
     const navigate = useNavigate();
