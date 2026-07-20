b6f0444fa640b38ef7ae1a8a9bd0a1e79dca7a2d
Alex42 <alexcrazy42@mail.ru>
2026-07-20 21:46:02 +0300

    continue e2e tests

--- test/e2e/README.md
+++ test/e2e/README.md
@@ -12,9 +12,31 @@ Page Object Model - инкапсулируем взаимодействие со
 
 ## Инструменты и окружение
 
-Инструменты: playwright
+Инструменты: playwright (@playwright/test)
 Окружение: на локалке поднятие через docker-compose, а в основном - через дебаг и стейдж
 Формирование отчетов: Xray (Jira), Allure Report, Report Portal
+
+Запуск тестов с шардингом: npx playwright test --shard=1/3
+
+browser - singleton на уровне одного worker
+context/page - scoped для каждого теста и создаются из этого singleton browser
+
+
+## Получение элементов со страницы
+
+Мы не хотим переписывать тесты при малейшем изменении верстки, смене языка, смене фреймворка, замене ui библиотеки и тд, поэтому не получаем элемент со страницы через:
+
+1. название внутри элемента
+2. классы
+3. классы ui библиотек, которые могут меняться со сменой версии
+4. CSS-селекторы по структуре
+5. nth(0) (порядок элементов)
+
+Вместо этого используем:
+
+1. data-testid
+2. При невозможности добавить data-testid добавляем комбинацию ролей: кнопка + название
+3. Ищем элементы в родительских элементах с четким  (кнопка в модалке по названию)
 
 ## Примеры, что проверяем
 
@@ -39,14 +61,15 @@ Idempotency:
 ## Allure
 
 Референсы:
-
 https://habr.com/ru/companies/ru_mts/articles/720692
 https://habr.com/ru/companies/clevertec/articles/822583/
 
-## TODO
+## UI элементы
 
 input, date, select, range, checkbox, radio, file, image, video
 button, color, datetime-local, month, week, time, search, password, email, number, textarea
+
++ валидация форм
 
 search with suggestion
 date picker/календарь (не нативный)
@@ -55,7 +78,7 @@ Drag-and-Drop
 File Upload with Preview
 Modal / Dialog / Popup
 Tabs / Accordion / Collapsible
-Pagination / Пагинация (именно в плане поиска на странице нужного элемента)
+Pagination / Пагинация / Filters (именно в плане поиска на странице нужного элемента)
 Infinite Scroll / Ленивая загрузка
 Toast / Notification / Snackbar
 Progress Bar / Индикатор загрузки
@@ -65,18 +88,14 @@ Chip / Tag Input
 Captcha / reCAPTCHA
 Tooltip / Popover
 Stepper / Wizard (Пошаговый мастер)
+Confirmation Dialog / Skeleton Screen / Undo Redo
+Rich Text Editor WYSIWYG / Auto-save текста / Character Word Counter / Copy to Clipboard
+Breadcrumbs / Sticky Header / Back to Top / Sidebar Drawer
 
+## TODO
 
-Суть:
-В пайплайне (GitLab CI / GitHub Actions) запускай Playwright не последовательно, а с шардингом:
-npx playwright test --shard=1/3
-
-browser - singleton
-context/page - scoped для каждого теста и создаются из этого singleton browser
-
-- тест привычных простых страниц и гридов
-- дебоунс ввод понемногу (чтобы тестить кол-во запросов)
-- перехват трафика и мок апи запросов
-- проверка что браузер делает не больше n запросов
+- тест привычных простых страниц и гридов +
+- дебоунс ввод понемногу (чтобы тестить кол-во запросов) +
+- перехват трафика и мок апи запросов +
 - тест асинхронных вебсокетов в чате
 - тест загрузки видео
--- test/e2e/tests/fithub.equipment.spec.ts
+++ test/e2e/tests/fithub.equipment.spec.ts
@@ -1,7 +1,15 @@
-﻿import { BACKEND_URLS } from "../config/sites";
+﻿import { Page } from "@playwright/test";
+import { BACKEND_URLS } from "../config/sites";
 import { expect, test } from "../fixtures/fixtures";
-import { EquipmentPage, IEquipmentCreateModel } from "../pages/EquipmentPage";
+import { EquipmentPage, IEquipmentCreateOrEditModel } from "../pages/EquipmentPage";
 import { expectNoRequest } from "../utils/pageExtensions";
+
+const  createEquipment = async (cmsAdminPage: Page, model: IEquipmentCreateOrEditModel): Promise<void> => {
+    const page = new EquipmentPage(cmsAdminPage);
+    await page.open();
+
+    await page.create(model);    
+}
 
 test('equipment create', async ({ cmsAdminPage }) => {
     const responsePromise = cmsAdminPage.waitForResponse(
@@ -9,10 +17,7 @@ test('equipment create', async ({ cmsAdminPage }) => {
             && resp.request().method() == 'POST'
     );
 
-    const page = new EquipmentPage(cmsAdminPage);
-    await page.open();
-
-    const createModel : IEquipmentCreateModel = {
+    const createModel : IEquipmentCreateOrEditModel = {
         brand: 'TestBrand',
         name: 'test equipment',
         description: 'test equipment description',
@@ -21,7 +26,7 @@ test('equipment create', async ({ cmsAdminPage }) => {
         isActive: true
     }
 
-    await page.create(createModel);
+    await createEquipment(cmsAdminPage, createModel);
 
     const response = await responsePromise;
     expect(response.status()).toBe(200);
@@ -36,11 +41,95 @@ test('equipment create', async ({ cmsAdminPage }) => {
 })
 
 test('equipment edit', async ({ cmsAdminPage }) => {
-    throw new Error('not implemented')
+    const responsePromise = cmsAdminPage.waitForResponse(
+        resp => resp.url().includes('/api/v1/equipments')
+            && resp.request().method() == 'POST'
+    );
+
+    const createModel : IEquipmentCreateOrEditModel = {
+        brand: 'TestBrand',
+        name: 'test equipment',
+        description: 'test equipment description',
+        addDescription: null,
+        instructionAddBefore: new Date(2026, 8, 1),
+        isActive: true
+    }
+
+    await createEquipment(cmsAdminPage, createModel);
+
+    const response = await responsePromise;
+    expect(response.status()).toBe(200);
+
+    const json = await response.json();
+    const newId = json['id'];
+
+    const eqPage = new EquipmentPage(cmsAdminPage);
+    await eqPage.openConcretePage(newId);
+
+    const updatePromise = cmsAdminPage.waitForResponse(
+        resp => resp.url().includes('/api/v1/equipments')
+            && resp.request().method() == 'PUT'
+    );
+
+    const updateModel : IEquipmentCreateOrEditModel = {
+        brand: 'TestBrand',
+        name: 'test equipment updated',
+        description: 'test equipment description updated',
+        addDescription: 'updated',
+        instructionAddBefore: new Date(2026, 8, 1),
+        isActive: true
+    }
+
+    await eqPage.edit(updateModel);
+
+    const updateResponse = await updatePromise;
+
+    expect(updateResponse.status()).toBe(200);
+
+    const updateJson = await updateResponse.json();
+    expect(updateJson['id']).toBe(newId);
+    expect(updateJson['name']).toBe(updateModel.name);
+    expect(updateJson['description']).toBe(updateModel.description);
+    expect(updateJson['additionalDescroption']).toBe(updateModel.addDescription);
+    expect(updateJson['isActive']).toBe(updateModel.isActive);
+
+    const url = BACKEND_URLS['fithub'];
+    const deleteResponse = await cmsAdminPage.request.delete(`${url}/api/v1/equipments/${newId}`)
+
+    expect(deleteResponse.status()).toBe(200);
 })
 
 test('equipment delete', async ({ cmsAdminPage }) => {
-    throw new Error('not implemented')
+    const responsePromise = cmsAdminPage.waitForResponse(
+        resp => resp.url().includes('/api/v1/equipments')
+            && resp.request().method() == 'POST'
+    );
+
+    const createModel : IEquipmentCreateOrEditModel = {
+        brand: 'TestBrand',
+        name: 'test equipment',
+        description: 'test equipment description',
+        addDescription: null,
+        instructionAddBefore: new Date(2026, 8, 1),
+        isActive: true
+    }
+
+    await createEquipment(cmsAdminPage, createModel);
+
+    const response = await responsePromise;
+    expect(response.status()).toBe(200);
+
+    const json = await response.json();
+    const newId = json['id'];
+
+    const eqPage = new EquipmentPage(cmsAdminPage);
+    await eqPage.open();
+    await eqPage.delete(newId);
+
+    const url = BACKEND_URLS['fithub'];
+
+    const getResponse = await cmsAdminPage.request.get(`${url}/api/v1/equipments/${newId}`);
+    expect(getResponse.status()).toBe(404);
 })
 
 test('should search brands only after typing at least 3 characters', async ({ cmsAdminPage }) => {
